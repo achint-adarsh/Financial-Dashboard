6461f27ac622ee6bc5bbc5465d6b14a63c5b5f98
achint-adarsh <adarshachint@gmail.com>
2026-04-04 00:26:48 +0530

    added API endpoints overview on root route

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
-    app.listen(5000, () => 
-        console.log("Server is running on port http://localhost:5000"));
+    app.listen(PORT, () => 
+        console.log(`Server is running on port http://localhost:${PORT}`));
 }).catch((error) => {
     console.error("Failed to connect to MongoDB", error);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import recordRoutes from "./routes/recordRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
-import {errorHandler} from "./middleware/errorMiddleware.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -15,9 +15,27 @@ app.use(cors());
 app.use(express.json());
 
 
-// app.use("/", (req,res) =>{
-//     res.status(200).json({message:"Welcome to the Financial Dashboard API"});
-// })
+app.use("/", (req, res) => {
+    res.status(200).json({
+        messge: "API is running",
+        auth: {
+            register: "POST /api/auth/register",
+            login: "POST /api/auth/login",
+            me: "GET /api/auth/me"
+        },
+
+        records: {
+            getAll: "GET /api/records",
+            create: "POST /api/records",
+            update: "PUT /api/records/:id",
+            delete: "DELETE /api/records/:id"
+        },
+
+        dashboard: {
+            summary: "GET /api/dashboard/summary"
+        }
+    });
+})
 
 app.use("/api/auth", authRoutes);
 app.use("/api/records", recordRoutes);
